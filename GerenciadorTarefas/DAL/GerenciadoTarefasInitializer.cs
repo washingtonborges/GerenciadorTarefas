232dbb6013ba5160f9fef2497df8e7c66c3be7ba
Washington Borges <washington.rborges@gmail.com>
2018-08-26 12:15:43 -0300

    Classe renponsável por inicializar o banco de dados com dados de teste

---
+++ GerenciadorTarefas/DAL/GerenciadoTarefasInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using GerenciadorTarefas.Models;
+
+namespace GerenciadorTarefas.DAL
+{
+    public class GerenciadorTarefasInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<GerenciadorTarefasContext>
+    {
+        protected override void Seed(GerenciadorTarefasContext context)
+        {
+            var usuarios = new List<Usuario>
+            {
+            new Usuario{Nome="admin", Email="admin@admin.br", Permissao="Administrador", Senha="admin"}
+            };
+
+            usuarios.ForEach(s => context.Usuario.Add(s));
+            context.SaveChanges();
+
+            var tarefas = new List<Tarefa>
+            {
+            new Tarefa{Titulo="Primeira tarefa",Concluida=false},
+            new Tarefa{Titulo="Segunda tarefa",Concluida=true}
+            };
+
+            tarefas.ForEach(s => context.Tarefa.Add(s));
+            context.SaveChanges();
+
+        }
+    }
+}
