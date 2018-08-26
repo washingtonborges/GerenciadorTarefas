7a107ba646c416c5380e753522d6e771a583dd76
Washington Borges <washington.rborges@gmail.com>
2018-08-26 11:56:16 -0300

    Criar contexto para a camada de acesso a dados(DAL)

---
+++ GerenciadorTarefas/DAL/GerenciadorTarefasContext.cs
@@ -0,0 +1,22 @@
+﻿using GerenciadorTarefas.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace GerenciadorTarefas.DAL
+{
+    public class GerenciadorTarefasContext : DbContext
+    {
+
+        public GerenciadorTarefasContext() : base("GerenciadorTarefasContext")
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Tarefa> Tarefa { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
