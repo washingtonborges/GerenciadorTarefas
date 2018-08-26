07afb12ab330ea8bd5075107b616ee38e5df2aaf
Washington Borges <washington.rborges@gmail.com>
2018-08-26 14:51:32 -0300

    Validar usuario e senha - Script de submit do formulário de login; Recuperar resposta json da aplicação via ajax para redirecionar ou exibir mensagem de erro na tentativa de login invalida

---
+++ GerenciadorTarefas/Scripts/custom.js
@@ -0,0 +1,32 @@
+﻿$('.form-signin').on('submit', function () {
+    $.ajax({
+        url: "/Home/Login"
+        ,
+        type: "POST"
+        ,
+        data: JSON.stringify({
+            Usuario: {
+                Nome: $("#username").val()
+                ,
+                Senha: $("#password").val()
+            }
+        })
+        ,
+        contentType: 'application/json; charset=utf-8'
+        ,
+        success: function () {
+            window.location = "/Tarefas"
+        }
+        ,
+        error: function (e) {
+            console.log(e)
+            showError(e.responseText)
+        }
+    })
+})
+
+function showError(errorMessage) {
+    var container = $('.alert');
+    container.removeClass('d-none');
+    container.html(errorMessage);
+}
