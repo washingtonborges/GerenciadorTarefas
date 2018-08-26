6fc0b4fb55c28f8a93278161feb8955f0cfe5a05
Washington Borges <washington.rborges@gmail.com>
2018-08-26 11:49:37 -0300

    Criar os modelos de dados

---
+++ GerenciadorTarefas/Models/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace GerenciadorTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+        public string Titulo { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
---
+++ GerenciadorTarefas/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorTarefas.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Permissao { get; set; }
+        public string Senha { get; set; }
+    }
+}
