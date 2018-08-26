950c0c7c3971d2498039b0bedbf1c3d1a4c7dac2
Washington Borges <washington.rborges@gmail.com>
2018-08-26 12:08:37 -0300

    Anotações nos modelos de dados para definir chaves primarias, auto incremento e validação de dados para uso posterior nos formulários

--- GerenciadorTarefas/Models/Tarefa.cs
+++ GerenciadorTarefas/Models/Tarefa.cs
@@ -1,8 +1,14 @@
-﻿namespace GerenciadorTarefas.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorTarefas.Models
 {
     public class Tarefa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int TarefaId { get; set; }
+        [Required]
         public string Titulo { get; set; }
         public bool Concluida { get; set; }
     }
--- GerenciadorTarefas/Models/Usuario.cs
+++ GerenciadorTarefas/Models/Usuario.cs
@@ -1,11 +1,22 @@
-﻿namespace GerenciadorTarefas.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorTarefas.Models
 {
     public class Usuario
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UsuarioId { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Email incorreto, favor digite um email válido.")]
         public string Email { get; set; }
+        [Required]
         public string Permissao { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
     }
 }
