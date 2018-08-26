f703dfba28cc9aaf6090de626790f05745f288eb
Washington Borges <washington.rborges@gmail.com>
2018-08-26 14:47:03 -0300

    Adicionado página JS customizada no layout padrão da aplicação

--- GerenciadorTarefas/App_Start/BundleConfig.cs
+++ GerenciadorTarefas/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace GerenciadorTarefas
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                      "~/Scripts/custom.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
