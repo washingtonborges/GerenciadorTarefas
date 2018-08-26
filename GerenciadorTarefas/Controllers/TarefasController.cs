435971fe58f6a0ec2ed4d45e29ad09c1f40c9bc4
Washington Borges <washington.rborges@gmail.com>
2018-08-26 12:35:51 -0300

    Criado CRUD básico com scaffold do modelo de dados Tarefa

---
+++ GerenciadorTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GerenciadorTarefas.DAL;
+using GerenciadorTarefas.Models;
+
+namespace GerenciadorTarefas
+{
+    public class TarefasController : Controller
+    {
+        private GerenciadorTarefasContext db = new GerenciadorTarefasContext();
+
+        // GET: Tarefas
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Tarefa.ToListAsync());
+        }
+
+        // GET: Tarefas/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = await db.Tarefa.FindAsync(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tarefas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "TarefaId,Titulo,Concluida")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarefa.Add(tarefa);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = await db.Tarefa.FindAsync(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "TarefaId,Titulo,Concluida")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarefa).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = await db.Tarefa.FindAsync(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Tarefa tarefa = await db.Tarefa.FindAsync(id);
+            db.Tarefa.Remove(tarefa);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
