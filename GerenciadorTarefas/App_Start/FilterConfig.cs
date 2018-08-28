932150468c8cd9f1bb1316399255e312e4515956
Washington Borges <washington.rborges@gmail.com>
2018-08-28 00:24:28 -0300

    Adicionado no FilterConfig atributo de configuração customizado para limitar acesso ao sistema

--- GerenciadorTarefas/App_Start/FilterConfig.cs
+++ GerenciadorTarefas/App_Start/FilterConfig.cs
@@ -1,5 +1,8 @@
-﻿using System.Web;
+﻿using GerenciadorTarefas.DAL;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace GerenciadorTarefas
 {
@@ -8,6 +11,55 @@ namespace GerenciadorTarefas
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeUserAttribute());
+        }
+    }
+    
+    public class AuthorizeUserAttribute : AuthorizeAttribute
+    {
+
+        private GerenciadorTarefasContext db = new GerenciadorTarefasContext();
+
+        // Custom property
+        public string Perfil { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool verificaPerfil = false;
+
+            var isAuthorized = base.AuthorizeCore(httpContext);
+            if (!isAuthorized)
+            {
+                verificaPerfil = false;
+            }
+            if (this.Perfil == null)
+            {
+                verificaPerfil = true;
+
+            } else {
+                var perfil = db.Perfil.FirstOrDefault(p => p.Nome == this.Perfil);
+                var usuario = db.Usuario.FirstOrDefault(u => u.Nome == httpContext.User.Identity.Name.ToString());
+                if (usuario != null && perfil != null)
+                {
+                    if (usuario.PerfilId == perfil.PerfilId)
+                    {
+                        verificaPerfil = true;
+                    }
+                }
+            }
+            return verificaPerfil;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                        new RouteValueDictionary(
+                            new
+                            {
+                            controller = "Home",
+                                action = "AcessoNegado"
+                            })
+                        );
         }
     }
 }
