cab09fd47db348e54d5c920639db401386e7e470
Washington Borges <washington.rborges@gmail.com>
2018-08-27 23:15:32 -0300

    Adicionado modelo do Perfil para gerenciar permissão de acesso

---
+++ GerenciadorTarefas/Models/Perfil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorTarefas.Models
+{
+    public class Perfil
+    {      
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PerfilId { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+    }
+
+}
